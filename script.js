62989d90a9a54eca3f7486b92adcc7d667fad335
Raphico <RaphicoGit@gmail.com>
2022-10-14 12:06:02 -0700

    download qr

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const qr = document.querySelector('.qrCode');
+const url = document.querySelector('#url');
 
-new QRCode(document.querySelector(".qr-code"), {
+let qrCode = new QRCode(qr, {
   text: "http://jindo.dev.naver.com/collie",
   width: 200,
   height: 200,
@@ -7,3 +9,44 @@ new QRCode(document.querySelector(".qr-code"), {
   colorLight : "#ffffff",
   correctLevel : QRCode.CorrectLevel.H
 });
+
+document.querySelector('.url-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (!url.value)
+  {
+    alert('Pls add a url to generate a qr code');
+  }
+  else
+  {
+    try
+    {
+      new URL(url.value);
+
+      generateQrCode(url.value);
+    }
+    catch (error)
+    {
+      alert('Not a valid url');
+    }
+    url.value = '';
+  }
+})
+
+document.querySelector('.download-btn').addEventListener('click', () => {
+  html2canvas(qr)
+    .then((canvas) => {
+      const url = canvas.toDataURL('image/png', 1.0);
+      const link = document.createElement('a');
+      link.setAttribute('download', 'qr-code');
+      link.setAttribute('href', url);
+      link.click();
+    })
+});
+
+const generateQrCode = (url) =>
+{
+  qrCode.clear();
+  qrCode.makeCode(url);
+}
+
